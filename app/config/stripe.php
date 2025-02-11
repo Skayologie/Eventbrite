c8d62fff61295e5cc3f73bd6bb02f8348dfbd8ce
Doja <dojaoualla@gmail.com>
2025-02-11 14:25:29 +0100

    branch de payment

---
+++ app/config/stripe.php
@@ -0,0 +1,6 @@
+
+<?php
+return [
+    'secret_key' => 'sk_test_51QqwsCKabufyYHDhhkwGsNPRFcskyaFR2FcAQGvRkzhzeaf7nCmzSX9RM43aIuRlkCgUmeYbk9mVw9slhMoNRWJP00dU51vbNx ', 
+    'publishable_key' => 'pk_test_51QqwsCKabufyYHDhVpKKAg45rfiN0lfgcCliKnRPbw1lBwIiJd29xctzpUShWr6Fdpb1JE3krNskcdFQZlQZuKRd00TyoNhGPn', 
+];
