e46bf77f93c61384b6eb4a68f285c4ac49d3983c
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-02-12 00:13:04 +0100

    finished the initial coding of the event class with its methods

--- app/models/Event.php
+++ app/models/Event.php
@@ -45,8 +45,98 @@ class Event{
         return $this->model->Add($table, $columns, $values);
     }
 
-    public function updateEvent(){
-        
+    public function updateEvent($event_id,$event_creator,$event_title,$event_description,$event_city,$event_region,$event_link,$event_price,$event_address,$event_capacity,$event_category,$event_type,$event_status,$event_date,$created_at,$promo_code,$available_seats,$thumbnail){
+        $table = 'events';
+        $data = [
+            "event_id"=>$event_id,
+            "event_creator"=>$event_creator,
+            "event_title"=>$event_title,
+            "event_description"=>$event_description,
+            "event_region"=>$event_region,
+            "event_city"=>$event_city,
+            "event_address"=>$event_address,
+            "event_link"=>$event_link,
+            "event_price"=>$event_price,
+            "event_capacity"=>$event_capacity,
+            "event_category"=>$event_category,
+            "event_type"=>$event_type,
+            "event_status"=>$event_status,
+            "event_date"=>$event_date,
+            "created_at"=>$created_at,
+            "promocode"=>$promo_code,
+            "available_seats"=>$available_seats,
+            "thumbnail"=>$thumbnail
+        ];
+
+        return $this->model->Edit($event_id, $table, $data);
     }
 
-}
+    
+    public function deleteEvent($event_id) {
+        $conn = $this->pdo;
+        $sql = "DELETE FROM events WHERE event_id = :event_id";
+        $stmt = $conn->prepare($sql);
+        return $stmt->execute([':event_id' => $event_id]);
+    }
+
+
+    public function show_events($conditions = []) {
+        $query = "SELECT events.*, users.name as event_creator, categories.category_name as category_name, users.photo
+            FROM events
+            LEFT JOIN users ON users.user_id = events.user_id
+            LEFT JOIN categories ON categories.id = events.event_category
+            LEFT JOIN region ON events.event_region = region.id
+            LEFT JOIN ville ON events.event_city = ville.id";
+        
+        if (!empty($conditions)) {
+            $whereConditions = [];
+            foreach ($conditions as $key => $val) {
+                $whereConditions[] = "$key = ?";
+            }
+            $query .= " WHERE " . implode(" AND ", $whereConditions);
+        }
+    
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute(array_values($conditions));
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+
+    public function search_events($searchTerm) {
+        $query = "SELECT events.*, users.name as event_creator, categories.category_name as category_name, users.photo
+            FROM events
+            LEFT JOIN users ON users.user_id = events.user_id
+            LEFT JOIN categories ON categories.id = events.event_category
+            LEFT JOIN region ON events.event_region = region.id
+            LEFT JOIN ville ON events.event_city = ville.id
+            WHERE events.event_title LIKE ?";
+        
+        $searchTermWithWildcards = "%" . $searchTerm . "%";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$searchTermWithWildcards]);
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function report_event($event_id, $user_id, $report_description) {
+        $query = "INSERT INTO reports (event_id, user_id, report_reason, report_description, created_at)
+                  VALUES (?, ?, ?, NOW())";
+        
+        $stmt = $this->pdo->prepare($query);
+        $result = $stmt->execute([$event_id, $user_id,$report_description]);
+        
+        return $result;
+    }
+    
+    
+    }
+    
+
+
+    
+
+
+
+    }
+
